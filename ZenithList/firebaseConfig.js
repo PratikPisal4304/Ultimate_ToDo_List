b6fef8d5364dbb860ccdd72746498601097bd5f2
Pratik Nitin Pisal <pisalpratik2004@gmail.com>
2025-07-19 14:47:04 +0530

    feat: Update Firebase configuration to use environment variables; enhance .gitignore to include .env files

--- ZenithList/firebaseConfig.js
+++ ZenithList/firebaseConfig.js
@@ -2,15 +2,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import Constants from 'expo-constants';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCOFyObRry8y13nFVCKIcQTMaV5m09Z9es",
-  authDomain: "zenithlist-661da.firebaseapp.com",
-  projectId: "zenithlist-661da",
-  storageBucket: "zenithlist-661da.firebasestorage.app",
-  messagingSenderId: "20447602088",
-  appId: "1:20447602088:web:39dae17fda093e5b6c699d"
+  apiKey: Constants.expoConfig.extra.firebaseApiKey,
+  authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
+  projectId: Constants.expoConfig.extra.firebaseProjectId,
+  storageBucket: Constants.expoConfig.extra.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig.extra.firebaseMessagingSenderId,
+  appId: Constants.expoConfig.extra.firebaseAppId,
 };
 
 // Initialize Firebase
